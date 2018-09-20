62e4a77f93352b14a67c8ae5da88ee520ac7c442
Yuri Egorov <ysegorov@gmail.com>
2018-09-20 08:56:16 +0300

    add initial postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,38 @@
+
+var debug = process.env.NODE_ENV === 'development';
+
+var plugins = [
+  require('postcss-import')({
+    // path: [
+    //   'node_modules/normalize.css/',
+    //   'node_modules/sanitize.css/',
+    // ]
+  }),
+  require('postcss-mixins')(),
+  require('postcss-custom-properties')({
+    preserve: false
+  }),
+  require('postcss-color-gray')(),
+  require('postcss-color-hwb')(),
+  require('postcss-color-function')(),
+  require('postcss-flexbugs-fixes')(),
+  require('autoprefixer')()
+];
+
+if (!debug) {
+  plugins.push(require('cssnano')({
+      normalizeUrl: false
+    })
+  );
+} else {
+  plugins.push(require('postcss-discard-comments')());
+}
+
+plugins.push(require('postcss-reporter')());
+
+module.exports = function () {
+  return {
+    plugins: plugins,
+    map: false
+  };
+};
